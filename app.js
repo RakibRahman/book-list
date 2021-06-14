3940e26c5cbda9edd789355fcda12649c2022923
RakibRahman <md.rr.talukder@gmail.com>
2021-06-15 00:36:50 +0600

    code updated

--- app.js
+++ app.js
@@ -5,16 +5,31 @@ const bookYear = form.bookYear;
 const submit = form.submit;
 const output = document.querySelector("#output");
 
-function getBook(e) {
-  e.preventDefault();
-  output.innerHTML += `
+const books = [];
 
-    <ul>
-        <li>Book Name: ${bookName.value}</li>
-        <li>Book Writer: ${bookAuthor.value}</li>
-        <li>Release Year: ${bookYear.value}</li>
-    
-    </ul>
-`;
+const addBooks = (name, author, year) => {
+  books.push({ name: name, author: author, year: year });
+  return { name, author, year };
+};
+
+const generateBooks = ({ name, author, year }) => {
+  output.innerHTML += `
+  
+     <div class="bookList">
+     <h3>${name}</h3>
+     <p>${author}</p>
+     <p>${year}</p>
+     </div>
+  `;
+};
+books.forEach(generateBooks); //loop through each book object and push it onto the innerHTML
+
+function generateData(e) {
+  e.preventDefault();
+  const newBook = addBooks(bookName.value, bookAuthor.value, bookYear.value);
+  generateBooks(newBook);
+  bookYear.value = "";
+  bookName.value = "";
+  bookAuthor.value = "";
 }
-submit.addEventListener("click", getBook);
+form.addEventListener("submit", generateData);
